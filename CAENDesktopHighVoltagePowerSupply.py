ecdcfba27c4021c091a9f3a0b3d4546782e91cb2
Matias Senger <m.senger@hotmail.com>
2021-05-06 15:48:50 +0200

    Improved default BD0 implementation

--- CAENDesktopHighVoltagePowerSupply.py
+++ CAENDesktopHighVoltagePowerSupply.py
@@ -14,7 +14,12 @@ def create_command_string(BD, CMD, PAR, CH=None, VAL=None):
 	
 
 class CAENDesktopHighVoltagePowerSupplyUSB:
-	def __init__(self, port=None):
+	# This class was implemented according to the specifications in the 
+	# user manual here: https://www.caen.it/products/dt1470et/
+	def __init__(self, port=None, default_BD0=True):
+		if default_BD0 not in [True, False]:
+			raise ValueError(f'The argument <default_BD0> must be either True of False. Received {default_BD0}.')
+		self.default_BD0 = default_BD0
 		if port is None:
 			if platform.system() == 'Linux':
 				port = '/dev/ttyACM0' # According to the user manual, this is the default port in most Linux distributions. I am on Ubuntu and it is the case.
@@ -31,17 +36,22 @@ class CAENDesktopHighVoltagePowerSupplyUSB:
 			timeout = 1,
 		)
 	
-	def send_command(self, CMD, PAR, CH=None, VAL=None, BD=0):
+	def send_command(self, CMD, PAR, CH=None, VAL=None, BD=None):
+		if BD is None:
+			if self.default_BD0 == True:
+				BD = 0
+			else:
+				raise ValueError(f'Please specify a value for the <BD> parameter. Refer to the CAEN user manual.')
 		self.serial_port.write(create_command_string(BD=BD, CMD=CMD, PAR=PAR, CH=CH, VAL=VAL).encode('ASCII'))
 	
 	def read_response(self):
 		return self.serial_port.read(9999).decode('ASCII')[:-2] # Remove the annoying '\r\n' in the end.
 	
-	def query(self, CMD, PAR, CH=None, VAL=None, BD=0):
+	def query(self, CMD, PAR, CH=None, VAL=None, BD=None):
 		self.send_command(BD=BD, CMD=CMD, PAR=PAR, CH=CH, VAL=VAL)
 		return self.read_response()
 
 if __name__ == '__main__':
 	source = CAENDesktopHighVoltagePowerSupplyUSB()
-	print(source.query(CMD='MON', PAR='VSET', CH=0))
+	print(source.query(CMD='MON', PAR='VSET', CH=0, BD=0))
 	
