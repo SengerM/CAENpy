05624ae0ac17d7f20609d50b1d1488b32e8c6843
Matias Senger <m.senger@hotmail.com>
2023-03-30 16:50:51 +0200

    Updated examples

--- examples/digitizer.py
+++ examples/digitizer.py
@@ -1,8 +1,6 @@
 from CAENpy.CAENDigitizer import CAEN_DT5742_Digitizer
-import numpy
 import pandas
 import plotly.express as px
-import time
 
 def configure_digitizer(digitizer:CAEN_DT5742_Digitizer):
 	digitizer.set_sampling_frequency(MHz=5000)
@@ -31,31 +29,32 @@ def convert_dicitonaries_to_data_frame(waveforms:dict):
 			data.append(df)
 	return pandas.concat(data)
 
-d = CAEN_DT5742_Digitizer(LinkNum=0)
-print('Connected with:',d.idn)
+if __name__ == '__main__':
+	d = CAEN_DT5742_Digitizer(LinkNum=0)
+	print('Connected with:',d.idn)
 
-configure_digitizer(d)
+	configure_digitizer(d)
 
-# Data acquisition ---
-with d:
-	waveforms = d.get_waveforms(get_ADCu_instead_of_volts=False) # Acquire the data.
+	# Data acquisition ---
+	with d:
+		waveforms = d.get_waveforms(get_ADCu_instead_of_volts=False) # Acquire the data.
 
-# Data analysis and plotting ---
-if len(waveforms) == 0:
-	raise RuntimeError('Could not acquire any event. The reason may be that you dont have anything connected to the inputs of the digitizer, or a wrong trigger threshold and/or offset setting.')
+	# Data analysis and plotting ---
+	if len(waveforms) == 0:
+		raise RuntimeError('Could not acquire any event. The reason may be that you dont have anything connected to the inputs of the digitizer, or a wrong trigger threshold and/or offset setting.')
 
-data = convert_dicitonaries_to_data_frame(waveforms)
+	data = convert_dicitonaries_to_data_frame(waveforms)
 
-print('Acquired data is:')
-print(data)
+	print('Acquired data is:')
+	print(data)
 
-fig = px.line(
-	title = 'CAEN digitizer testing',
-	data_frame = data.reset_index(),
-	x = 'Time (s)',
-	y = 'Amplitude (V)',
-	color = 'n_channel',
-	facet_row = 'n_event',
-	markers = True,
-)
-fig.write_html(f'plot.html')
+	fig = px.line(
+		title = 'CAEN digitizer testing',
+		data_frame = data.reset_index(),
+		x = 'Time (s)',
+		y = 'Amplitude (V)',
+		color = 'n_channel',
+		facet_row = 'n_event',
+		markers = True,
+	)
+	fig.write_html(f'plot.html')
---
+++ examples/digitizer_2.py
@@ -0,0 +1,37 @@
+from CAENpy.CAENDigitizer import CAEN_DT5742_Digitizer
+import pandas
+import plotly.express as px
+import time
+from digitizer import configure_digitizer, convert_dicitonaries_to_data_frame
+
+if __name__ == '__main__':
+	d = CAEN_DT5742_Digitizer(LinkNum=0)
+	print('Connected with:',d.idn)
+
+	configure_digitizer(d)
+
+	# Data acquisition ---
+	d.start_acquisition()
+	time.sleep(.5)
+	d.stop_acquisition()
+	waveforms = d.get_waveforms(get_ADCu_instead_of_volts=False) # Acquire the data.
+
+	# Data analysis and plotting ---
+	if len(waveforms) == 0:
+		raise RuntimeError('Could not acquire any event. The reason may be that you dont have anything connected to the inputs of the digitizer, or a wrong trigger threshold and/or offset setting.')
+
+	data = convert_dicitonaries_to_data_frame(waveforms)
+
+	print('Acquired data is:')
+	print(data)
+
+	fig = px.line(
+		title = 'CAEN digitizer testing',
+		data_frame = data.reset_index(),
+		x = 'Time (s)',
+		y = 'Amplitude (V)',
+		color = 'n_channel',
+		facet_row = 'n_event',
+		markers = True,
+	)
+	fig.write_html(f'plot.html')
